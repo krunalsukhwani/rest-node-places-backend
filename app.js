9d9b6d364218c8513eea4aac7ea74bda2e36b672
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-10-07 19:01:36 -0400

    Error Handling

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const HttpError = require("./models/http-error");
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    throw new HttpError("The requested URL was not found on this server.", 404);
+});
+
+//mandatory middleware to handle the error
+app.use((error, req, res, next) => {
+    if(res.headerSent){
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({message: error.message} || "An unknown error occured!");
+});
 
 mongoose
     .connect("mongodb+srv://ksukhwani:rX38DHqXieXx1avD@cluster0.lmhzv.mongodb.net/shared-places?retryWrites=true&w=majority&appName=Cluster0")
